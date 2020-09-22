52e2d787b241633d89f263a3c54685df1eed56ea
Patrick Mahloy <PatrickMahloy@Patricks-MBP.fios-router.home>
2020-09-22 19:02:01 -0400

    basic ajax call working

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('blip');
+var APIKey = "0cab6337530678cba09e0300bc133675";
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + "5809844" + "&APPID=" + APIKey;
+
+var Seattle = 5809844;
+var Dallas = 4684888;
+var Charleston = 4574324;
+
+
+
+$.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+        console.log(response)
+});
