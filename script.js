dc74bee41e449eb6ff8ecdb5b86ccd3300c8bfeb
Patrick Mahloy <PatrickMahloy@Patricks-MBP.fios-router.home>
2020-09-22 21:32:41 -0400

    got search field working with local storage

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 var APIKey = "0cab6337530678cba09e0300bc133675";
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + "5809844" + "&APPID=" + APIKey;
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
 var Seattle = 5809844;
 var Dallas = 4684888;
 var Charleston = 4574324;
 
+var city = $("#search").val();
 
+$(document).ready(function() {
+  $(".search").click(function(){
+    var input = $("#search").val();
+    console.log(input);
+    searchCity(input);
+  });
+  function searchCity(input) {
+    $.ajax({
+          url: "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=0cab6337530678cba09e0300bc133675",
+          method: "GET"
+        }).then(function(response) {
+            console.log(response)
+            if (history.indexOf(input) === -1) {
+              history.push(input);
+              window.localStorage.setItem("history", JSON.stringify(history));
+            }
+    });
+  }
+    const history = JSON.parse(window.localStorage.getItem("history")) || [];
+})
 
-$.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-        console.log(response)
-});
+// localstorage
