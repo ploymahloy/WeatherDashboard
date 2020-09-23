cb725c06517795e1f3b5463186e68183bb6994dd
Patrick Mahloy <PatrickMahloy@Patricks-MBP.fios-router.home>
2020-09-23 15:57:33 -0400

    weather icon displays on city search

--- script.js
+++ script.js
@@ -13,7 +13,12 @@ $(document).ready(function() {
     console.log(input);
     searchCity(input);
     $(`<button class="city">${input}</button>`).appendTo("nav");
-});
+  });
+  $(".search-btn").click(function(){
+    console.log("icon-test");
+    $("#icon").attr("src", "http://openweathermap.org/img/wn/10d@2x.png");
+  });
+
   function searchCity(input) {
     $.ajax({
           url: "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=0cab6337530678cba09e0300bc133675",
@@ -28,7 +33,5 @@ $(document).ready(function() {
   }
     const history = JSON.parse(window.localStorage.getItem("history")) || [];
 })
-
-
 
 // localstorage
